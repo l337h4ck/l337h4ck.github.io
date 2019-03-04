e61a3ea681bd8ac555012a081edd2f900b8f7636
l337h4ck <48199457+l337h4ck@users.noreply.github.com>
2019-03-04 02:35:46 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon"/>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>About</title>
+  <meta content="security researcher, security consultant, web security, penetration testing, cyber security, information security" name="keywords">
+  <meta content="Security Consultant" name="description">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+  <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="assets/lib/hover/hover.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/responsive.css" rel="stylesheet">
+  <link rel="shortcut icon" href="images/favicon.png">
+</head>
+<body>
+  <div id="header" class="home">
+    <div class="container">	
+      <div class="header-content">
+        <a href="index.php"><h1>About <span class="typed"></span></h1></a>
+        <p>We've began repairing tech as a hobby and it has now grown into our passion. <br> We offer only quality repairs at affordable prices to the local community.<br> Our mission is to make our customers happy and provide great results. <br> Our computer repair services are offered offsite or you can be dropped off.</p>
+        <ul style="list-style: none;">
+        	<li><p1><a href="index.php"">><p1> Go Back to Home </p1></a></li>
+        </ul><br>
+        <ul class="list-unstyled list-social">
+	  <!--<li><a href="https://github.com/" target="_blank"><i class="ion-social-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
+          <li><a href="https://twitter.com/" target="_blank"><i class="ion-social-twitter"></i></a></li>
+          -->
+          <li><a href="https://www.youtube.com/"  target="_blank"><i class="ion-social-youtube"></i></a></li>
+		  <li><a href="https://www.instagram.com/" target="_blank"><i class="ion-social-instagram"></i></a></li>
+		  <li><a href="https://www.facebook.com/" target="_blank"><i class="ion-social-facebook"></i></a></li>
+      </div>
+    </div>
+  </div>
+  <script src="assets/lib/jquery/jquery.min.js"></script>
+  <script src="assets/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/lib/typed/typed.js"></script>
+  <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="assets/lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="assets/lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon"/>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Contact Us</title>
+  <meta content="security researcher, security consultant, web security, penetration testing, cyber security, information security" name="keywords">
+  <meta content="Security Consultant" name="description">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+  <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="assets/lib/hover/hover.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/responsive.css" rel="stylesheet">
+  <link rel="shortcut icon" href="images/favicon.png">
+</head>
+<body>
+  <div id="header" class="home">
+    <div class="container">	
+      <div class="header-content">
+        <a href="index.php"><h1>Contact us for <span class="typed"></span></h1></a>
+        <p>To find out more about the services we offer or for pricing, please fill out the form below. We'll get in touch with you in under 24 hours.</p>
+        <ul style="list-style: none;">
+        	<a href="https://goo.gl/forms/VTwnidPYTpugvq1E3" target="_blank_">><p1> To Contact Us: Click Here </p1></a>
+        </ul><br>
+        <ul class="list-unstyled list-social">
+	  <!--<li><a href="https://github.com/" target="_blank"><i class="ion-social-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
+          <li><a href="https://twitter.com/" target="_blank"><i class="ion-social-twitter"></i></a></li>
+          -->
+          <li><a href="https://www.youtube.com/"  target="_blank"><i class="ion-social-youtube"></i></a></li>
+		  <li><a href="https://www.instagram.com/" target="_blank"><i class="ion-social-instagram"></i></a></li>
+		  <li><a href="https://www.facebook.com/" target="_blank"><i class="ion-social-facebook"></i></a></li>
+      </div>
+    </div>
+  </div>
+  <script src="assets/lib/jquery/jquery.min.js"></script>
+  <script src="assets/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/lib/typed/typed.js"></script>
+  <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="assets/lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="assets/lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
